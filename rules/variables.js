509063bac2ddf002d2129fbc1abe0ab11721238e
Joshua Wiens <joshuaw@easymetrics.com>
2017-03-05 11:44:47 -0600

    feat: update no-use-before-define for func-style update

--- rules/variables.js
+++ rules/variables.js
@@ -46,6 +46,6 @@ module.exports = {
 
     // disallow the use of variables before they are defined
     // http://eslint.org/docs/rules/no-use-before-define
-    'no-use-before-define': 'error'
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true  }],
   }
 };
