d2c280998c2e772f05f700c4837d7687c15b8494
Joshua Wiens <joshuaw@easymetrics.com>
2017-04-07 20:23:53 -0500

    fix: set import/extensions to warn from error
     - Reasoning: All of our loaders / plugins require webpack, though most have either a loose or no dependency on it thus this rule will almost always fail.

--- rules/imports.js
+++ rules/imports.js
@@ -138,7 +138,7 @@ module.exports = {
 
     // Ensure consistent use of file extension within the import path
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    'import/extensions': ['error', 'always', {
+    'import/extensions': ['warn', 'always', {
       js: 'never',
       jsx: 'never',
     }],
